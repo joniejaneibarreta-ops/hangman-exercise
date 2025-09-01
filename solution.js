bf75f0e0e0a3533b5faf4bc35e87c9233a7a27a1
joniejaneibarreta-ops <joniejane.ibarreta@hcdc.edu.ph>
2025-09-01 14:46:52 +0800

    Update solution.js
    Jonie Jane Ibarreta

--- solution.js
+++ solution.js
@@ -1,23 +1,37 @@
 import promp from "./promp.js";
 import data from "./data.js";
 
-/* Get one random word from data */
-const word = data[Math.floor(Math.random() * data.length)];
+/* Pick one random word from data */
+const word = data[Math.floor(Math.random() * data.length)]; 
+    const HANGMAN = "HANGMAN";
+    let wrongGuesses = 0;
 
-async function runGame () {
-    const answer =  await promp(`${word.question} `);
+async function runGame() {
 
-    if (answer.toLowerCase() !== word.word) {
+  while (wrongGuesses < HANGMAN.length) { {
 
-    } else {
-        console.log("Congratulations! You've guessed the word correctly.");
-        return;
+    const answer = await promp(`${word.question} `);
+
+    wrongGuesses++;
+
+    if (answer.toLowerCase() === word.word.toLowerCase()) {
+      console.log("Congratulations! You've guessed the word correctly!");
+      return;
     }
+    
+    console.log(HANGMAN.slice(0, wrongGuesses));
 
-
-    return runGame();
+    if (wrongGuesses === HANGMAN.length - 1) {
+      console.log(`Hint: ${word.hint}`);
+    }
+    
+    if (wrongGuesses === HANGMAN.length) {
+      console.log(`Game Over! The correct word was: ${word.word}`);
+      return;
+    }
+  }
+  return runGame();
 }
-
-
+}
 
 runGame();
